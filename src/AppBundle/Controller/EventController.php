fa9d3efaa4846374752eccf82a6c7be439c27482
Marko Stefanovic <markostefanovic941@gmail.com>
2018-02-11 21:45:33 +0100

    Created event list

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -2,40 +2,171 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\BlogPost;
-//use Doctrine\DBAL\Types\TextType;
 use AppBundle\Entity\Event;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-//use Sonata\AdminBundle\Form\Type\Filter\ChoiceType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\VarDumper\VarDumper;
 
-
+/**
+ * Event controller.
+ *
+ * @Route("event")
+ */
 class EventController extends Controller
 {
     /**
-     * @Route("/events/add", name="events_add")
+     * Lists all event entities.
+     *
+     * @Route("/", name="event_index")
+     * @Method("GET")
      */
-    public function addEventAction(Request $request)
+    public function indexAction()
+    {
+        dump('a');
+        $em = $this->getDoctrine()->getManager();
+
+        $events = $em->getRepository('AppBundle:Event')->findAll();
+
+        $date = '2017-12-06';
+        // End date
+        $end_date = '2018-4-31';
+        $dates_array = [];
+        while(strtotime($date) < strtotime($end_date))
+        {
+            $dates_array[] = $date = date ("Y-m-d", strtotime("+1 day", strtotime($date)));
+        }
+
+
+        return $this->render('event/index.html.twig', array(
+            'events' => $events,
+            'dates_array' => array_reverse( $dates_array),
+        ));
+    }
+
+    /**
+     * Creates a new event entity.
+     *
+     * @Route("/new", name="event_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
     {
         $event = new Event();
-        $form = $this -> createFormBuilder($event)
-            -> add('name',TextType::class,array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
-            -> add('category',TextType::class,array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
-            -> add('description',TextareaType::class,array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
-            -> add('priority',ChoiceType::class,array('choices' => array('Low' => 'Low', 'Normal'=>'Normal','High'=>'High'),'attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
-            -> add('date',DateTimeType::class,array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
-            ->getForm();
-
+        $form = $this->createForm('AppBundle\Form\EventType', $event);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
-            die('Submitted');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+
+            return $this->redirectToRoute('event_show', array('id' => $event->getId()));
         }
-        return $this->render('events/create.html.twig',array('form'=>$form->createView()));
+
+        return $this->render('event/new.html.twig', array(
+            'event' => $event,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a event entity.
+     *
+     * @Route("/{id}", name="event_show")
+     * @Method("GET")
+     */
+    public function showAction(Event $event)
+    {
+        $deleteForm = $this->createDeleteForm($event);
+
+        return $this->render('event/show.html.twig', array(
+            'event' => $event,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing event entity.
+     *
+     * @Route("/{id}/edit", name="event_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Event $event)
+    {
+        $deleteForm = $this->createDeleteForm($event);
+        $editForm = $this->createForm('AppBundle\Form\EventType', $event);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('event_edit', array('id' => $event->getId()));
+        }
+
+        return $this->render('event/edit.html.twig', array(
+            'event' => $event,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a event entity.
+     *
+     * @Route("/{id}", name="event_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Event $event)
+    {
+        $form = $this->createDeleteForm($event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($event);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('event_index');
+    }
+
+    /**
+     * Creates a form to delete a event entity.
+     *
+     * @param Event $event The event entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Event $event)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('event_delete', array('id' => $event->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+
+    /**
+     * @Route("/updateEventDate", name="update_event_date")
+     * @Method("POST")
+     */
+    public function updateEventDateAction(Request $request)
+    {
+        if($request->request->get('updateDate'))
+        {
+            //dump($request);
+            $em = $this->getDoctrine()->getManager();
+
+            $event = $em->getRepository('AppBundle:Event')->find($request->request->get('updateDate')['id']);
+            $newDate = new \DateTime($request->request->get('updateDate')['newDate']);
+            $event->setDate($newDate);
+            $em->flush();
+        }
+
+        dump("B");
+        return  new JsonResponse("{a: 'a'}");
     }
 }
